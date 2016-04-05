d1f4eeb4485d2af7a9e4ca5a2b786d9b089a66f4
Ron Gross <chiefninjaofficer@gmail.com>
2016-04-05 04:45:45 +0300

    Trying to add my first template... not working

--- web/views.py
+++ web/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Ready to turn some webpages into workflowies?")
+    #return HttpResponse("Ready to turn some webpages into workflowies?")
+	template = loader.get_template('web/index.html')
+
+	return HttpResponse(template.render(context, request))
