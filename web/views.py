732c98ab03a54689d9b1d6b5d5195e95257ca556
Ron Gross <chiefninjaofficer@gmail.com>
2016-04-05 04:53:26 +0300

    Link to github

--- web/views.py
+++ web/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Ready to turn some webpages into workflowies?")
+    return HttpResponse("Ready to turn some webpages into workflowies?<br/>Go to https://github.com/ripper234/Web2Workflowy to contribute.")
