0c7cd3d2e680b9f6057d632767fd2f98bdd269ec
Ron Gross <ron@mastercoin.org>
2016-04-05 03:30:04 +0300

    Fix STATICFILES

--- web2workflowy/settings.py
+++ web2workflowy/settings.py
@@ -115,7 +115,19 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+
+################################################
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
